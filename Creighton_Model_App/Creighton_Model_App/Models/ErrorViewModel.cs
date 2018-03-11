cd31e4ec2daf300fbc62717acedf8651aa9f998f
Robbie Shock <rob.f.shock@gmail.com>
2018-03-10 23:46:27 -0800

    initial file structure build

---
+++ Creighton_Model_App/Creighton_Model_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Creighton_Model_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
