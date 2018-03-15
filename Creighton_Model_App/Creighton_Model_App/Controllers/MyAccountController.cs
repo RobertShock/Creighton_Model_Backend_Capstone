0557717fbc15d07b8f6689e3f6b20a0deb989643
Robbie Shock <rob.f.shock@gmail.com>
2018-03-15 13:33:04 -0700

    redirected user to the home page after login

--- Creighton_Model_App/Creighton_Model_App/Controllers/MyAccountController.cs
+++ Creighton_Model_App/Creighton_Model_App/Controllers/MyAccountController.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Creighton_Model_App.Data;
+using Creighton_Model_App.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Creighton_Model_App.Controllers
 {
     public class MyAccountController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MyAccountController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
             return View();
         }
     }
