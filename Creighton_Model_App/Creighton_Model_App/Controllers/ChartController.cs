3848fc0eb14a341c4e7af1ff09c80767821b67e1
Robbie Shock <rob.f.shock@gmail.com>
2018-03-19 08:53:22 -0700

    added additional information to MyAccount, changed nav and added MyChart page

--- Creighton_Model_App/Creighton_Model_App/Controllers/ChartController.cs
+++ Creighton_Model_App/Creighton_Model_App/Controllers/ChartController.cs
@@ -3,6 +3,42 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Creighton_Model_App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Creighton_Model_App.Models;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Creighton_Model_App.Controllers
+{
+    public class ChartController : Controller
+    {  
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChartController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return View();
+        }
+    }
+}
+
+
+
+
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Creighton_Model_App.Models;
 
@@ -10,9 +46,11 @@ namespace Creighton_Model_App.Controllers
 {
     public class ChartController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public IActionResult Index()
         {
-            return View();
+            return View(db.Chart.ToList());
         }
     }
 }
